6427038bf0811ea732b226dea1565364c0a514c6
Tony Alvarez <tonyalva87@gmail.com>
2022-07-11 13:48:24 -0700

    resolve issue with simple render of app component

--- part-1/index.js
+++ part-1/index.js
@@ -1,9 +1,11 @@
-class FirstComponent extends React.Component {
-  render() {
-    return /*#__PURE__*/React.createElement("h1", null, "My Very First Component");
-  }
+const FirstComponent = () => {
+  return /*#__PURE__*/React.createElement("h1", null, "My Very First Component");
+};
 
-}
+const App = () => {
+  /*#__PURE__*/
+  React.createElement("div", null, /*#__PURE__*/React.createElement(FirstComponent, null));
+};
 
-ReactDOM.render( /*#__PURE__*/React.createElement(FirstComponent, null), document.getElementById("root"));
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
 
--- part-1/index.jsx
+++ part-1/index.jsx
@@ -1,8 +1,12 @@
-class FirstComponent extends React.Component {
-  render() {
-    return <h1>My Very First Component</h1>
-  }
+const FirstComponent = () => {
+  return <h1>My Very First Component</h1>
 }
 
-ReactDOM.render(<FirstComponent/>,
+const App = () => {
+  <div>
+    <FirstComponent />
+  </div>
+}
+
+ReactDOM.render(<App />,
   document.getElementById("root"));
