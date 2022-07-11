8aba0f1e0062e28d7ce43e238e4056a4abd27f7e
Tony Alvarez <tonyalva87@gmail.com>
2022-07-11 13:12:48 -0700

    add first-component

---
+++ part-1/index.js
@@ -0,0 +1,9 @@
+class FirstComponent extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("h1", null, "My Very First Component");
+  }
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(FirstComponent, null), document.getElementById("root"));
+
---
+++ part-1/index.jsx
@@ -0,0 +1,8 @@
+class FirstComponent extends React.Component {
+  render() {
+    return <h1>My Very First Component</h1>
+  }
+}
+
+ReactDOM.render(<FirstComponent/>,
+  document.getElementById("root"));
